859e7d2f5da8c3d1a5eac54997aa244fb8ac4dcb
Your Name <slayycasm997@gmail.com>
2026-02-23 09:55:41 +0530

    cleaned main branch

--- strategy/normalizer.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def normalize(series: pd.Series) -> pd.Series:
-    vmin, vmax = series.min(), series.max()
-    if vmax == vmin:  # constant column → all equal score
-        return pd.Series([1.0] * len(series), index=series.index)
-    normed = 1 - (series - vmin) / (vmax - vmin)
-    return normed.fillna(0.0)  # just in case
